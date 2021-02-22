19a90932bea4097d8012f40fb27b60ae076441cb
fajri <achmad17nov@gmail.com>
2021-02-22 17:34:38 +0700

    first commit;

--- app.js
+++ app.js
@@ -15,22 +15,18 @@ app.options('*', cors())
 //middleware
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
-app.use(authJwt());
+// app.use(authJwt());
 app.use('/public/uploads', express.static(__dirname + '/public/uploads'));
 app.use(errorHandler);
 
 //Routes
-const categoriesRoutes = require('./routes/categories');
-const productsRoutes = require('./routes/products');
 const usersRoutes = require('./routes/users');
-const ordersRoutes = require('./routes/orders');
+const containersRoutes = require('./routes/containers')
 
 const api = process.env.API_URL;
 
-app.use(`${api}/categories`, categoriesRoutes);
-app.use(`${api}/products`, productsRoutes);
 app.use(`${api}/users`, usersRoutes);
-app.use(`${api}/orders`, ordersRoutes);
+app.use(`${api}/containers`, containersRoutes);
 
 //Database
 mongoose.connect(process.env.CONNECTION_STRING, {
@@ -50,7 +46,7 @@ mongoose.connect(process.env.CONNECTION_STRING, {
 //     console.log('server is running http://localhost:3000');
 // })
 
-let server = app.listen(process.env.PORT || 3000, ()=>{
+let server = app.listen(process.env.PORT || 5006, ()=>{
     let port = server.address().port;
     console.log("Express is working on port " + port);
 })
